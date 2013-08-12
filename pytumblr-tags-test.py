3d08353957e74ceeba9ffe0b6785d17e9c3271b9
Christopher Su <christophersu9@gmail.com>
2013-08-12 22:25:40 +0800

    removed non tools

--- pytumblr-tags-test.py
+++
@@ -1,9 +0,0 @@
-def main():
-	params = {'tags': ['omg', 'nice']}
-	# params['tags'] = ",".join(params['tag']s'])
-	# params['tags'] = ', '.join([x.strip() for x in params['tags'].split(',')])
-	params['tags'] = ', '.join([i.split(',') for i in params['tags']])
-	print params['tags']
-
-if __name__ == '__main__':
-    main()
